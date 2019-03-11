8383473057f9e2084eabc4ff76d88d04b8fd2785
Roman Batsenko <rbatsenko@gmail.com>
2019-03-11 20:18:24 +0100

    signing in and registering backend

--- server.js
+++ server.js
@@ -2,6 +2,64 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.listen(3000);
+const database = {
+  users: [
+    {
+      id: 123,
+      name: 'John',
+      email: 'john@email.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 124,
+      name: 'Sally',
+      email: 'sally@email.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ],
+}
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json('Success!');
+  } else {
+    res.status(400).json('error logging in!');
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: 125,
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.listen(3000, () => {
+  console.log('App is running on port 3000!');
+});
+
+/*
+
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
